702b6e0dfdd95bd7c4e1ea388adcea6ec1787887
xiezik <xie13318782539@163.com>
2025-11-15 15:29:56 +0800

    stage0_height

--- legged_gym/legged_gym/envs/base/humanoid_robot.py
+++ legged_gym/legged_gym/envs/base/humanoid_robot.py
@@ -628,7 +628,7 @@ class HumanoidRobot(BaseTask):
         noisy_commands = self.commands[:, 0:3] * self.commands_scale
 
         # print(f"noisy_ang_vel: {noisy_ang_vel}")
-        # print(f"self.commands[:, 0:3]: {self.commands[:, 0:3]}")
+        # print(f"self.commands[:, 0:5]: {self.commands[:, 0:5]}")
         
         obs_buf = torch.cat((
                             #skill_vector, 
@@ -897,8 +897,8 @@ class HumanoidRobot(BaseTask):
     
     def _resample_commands(self, env_ids):
         set_x = torch.rand(len(env_ids), 1).to(self.device)
-        is_height = set_x < 1/3
         is_vel = set_x > 1/2
+        is_height = set_x < 1/3
         if self.cfg.commands.height_adaptive_speed:
             adaptive_speeds = self._generate_adaptive_speed(env_ids)
             self.commands[env_ids, 0] = adaptive_speeds
@@ -1069,7 +1069,7 @@ class HumanoidRobot(BaseTask):
             
             elif curriculum_cfg.success_mode == 'vel_tracking':
                 # 速度模式：判断是否达到指定速度
-                is_success = (self.episode_sums["tracking_x_vel"][env_id] / self.max_episode_length) > (0.6 * self.reward_scales["tracking_x_vel"])
+                is_success = (self.episode_sums["tracking_x_vel"][env_id] / self.max_episode_length) > (0.7 * self.reward_scales["tracking_x_vel"])
                 success_threshold = curriculum_cfg.velocity_success_threshold
                 failure_threshold = curriculum_cfg.velocity_failure_threshold
             
@@ -1652,9 +1652,9 @@ class HumanoidRobot(BaseTask):
             spacing = 0.01  # 采样间距 0.01m
             # 计算采样范围（确保中心对称）
             x_start = - num_x / 2 * spacing + 0.01
-            y_start = - num_y / 2 * spacing + 10
+            y_start = - num_y / 2 * spacing
             x_end = -x_start + 0.08
-            y_end = -y_start + 20
+            y_end = -y_start
 
             x_samples = torch.linspace(x_start, x_end, num_x, device=self.device)
             y_samples = torch.linspace(y_start, y_end, num_y, device=self.device)
@@ -2216,9 +2216,9 @@ class HumanoidRobot(BaseTask):
             spacing = 0.01  # 采样间距 0.01m
             # 计算采样范围（确保中心对称）
             x_start = - num_x / 2 * spacing + 0.01
-            y_start = - num_y / 2 * spacing + 10
+            y_start = - num_y / 2 * spacing
             x_end = -x_start + 0.08
-            y_end = -y_start + 20
+            y_end = -y_start
             
 
             x_samples = torch.linspace(x_start, x_end, num_x, device=self.device)
--- legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
+++ legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
@@ -56,7 +56,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
     class env(LeggedRobotCfg.env):
         num_envs = 2048
         num_dofs = 27     # 机器人总自由度：全身27个关节
-        episode_length_s = 4 #与课程学习有关 
+        episode_length_s = 20 #与课程学习有关 
         
         n_scan = 225
         n_priv = 3
@@ -84,7 +84,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         failure_threshold = 2  # 连续失败次数阈值
          
         # 存活时间模式参数
-        survival_time_threshold = 4  # 存活时间阈值（秒）
+        survival_time_threshold = 20  # 存活时间阈值（秒）
         survival_success_threshold = 2  # 连续存活成功次数阈值
         survival_failure_threshold = 3   # 连续存活失败次数阈值
         
@@ -246,10 +246,10 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         speed_gradient_weight = 0.4   # 高度梯度权重  
         speed_roughness_weight = 0.2  # 地形粗糙度权重
         class ranges( LeggedRobotCfg.commands.ranges ):
-            lin_vel_x = [0.8, 1.2] # min max [m/s]
-            lin_vel_y = [-0.0, 0.0]   # min max [m/s]
+            lin_vel_x = [-0.8, 1.2] # min max [m/s]
+            lin_vel_y = [-0.5, 0.5]   # min max [m/s]
             ang_vel_yaw = [-0.8, 0.8]    # min max [rad/s]
-            heading = [-0.0, 0.0]
+            heading = [-1.0, 1.0]
             height = [-0.5, 0.0]
                         
     class rewards(LeggedRobotCfg.rewards):
