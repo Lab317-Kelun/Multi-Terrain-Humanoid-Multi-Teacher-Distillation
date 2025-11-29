f716b6680830fefe926e349a1ced661c7a0839e7
xiezik <xie13318782539@163.com>
2025-11-29 18:02:41 +0800

    stage0_target_navigation_commonobs

--- legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
+++ legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
@@ -61,7 +61,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         n_scan = 225
         n_priv = 3
         n_priv_latent = 4 + 1 + 12 + 12  # 潜在状态维度
-        n_proprio = 75  # 实际obs_buf维度：3(commands)+3(ang_vel)+1(delta_yaw)+1(delta_pose_x)+1(delta_pose_y)+3(gravity)+27(dof_pos)+27(dof_vel)+12(action_history)=78
+        n_proprio = 78  # 实际obs_buf维度：3(commands)+3(ang_vel)+1(delta_yaw)+1(delta_pose_x)+1(delta_pose_y)+3(gravity)+27(dof_pos)+27(dof_vel)+12(action_history)=78
         history_len = 10
         
         # 重新计算总观测维度
@@ -261,8 +261,6 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
             tracking_y_vel = 1.
             tracking_ang_vel = 2.0
             heading_tracking = 1.0
-            
-            terrain_onehot_similarity = 0.5
               
             lin_vel_z = -0.5
             ang_vel_xy = -0.025
