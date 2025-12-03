2c5fca83943bdfc94b7388f46b2f913d7bec6c71
xiezik <xie13318782539@163.com>
2025-12-03 21:11:44 +0800

    stage0.5_target_navigation_newobs

--- legged_gym/legged_gym/scripts/play.py
+++ legged_gym/legged_gym/scripts/play.py
@@ -69,7 +69,7 @@ def play(args):
     env_cfg.commands.ranges.lin_vel_x = [0.5, 0.6]
     env_cfg.commands.ranges.lin_vel_y = [-0.0, 0.0]
     env_cfg.commands.ranges.ang_vel_yaw = [-0.0, 0.0]
-    env_cfg.commands.ranges.heading = [-0.0, 0.0]
+    env_cfg.commands.ranges.heading = [-1.0, 1.0]
     env_cfg.commands.ranges.height = [-0.0, 0.0]
     
     env_cfg.terrain.num_rows = 2
