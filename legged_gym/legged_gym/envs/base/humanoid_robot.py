23889ffca9d1da4f57ceb3ac366374e3d2eebb2b
xiezik <xie13318782539@163.com>
2025-12-03 22:51:10 +0800

    stage1_target_navigation_newobs

--- legged_gym/legged_gym/envs/base/humanoid_robot.py
+++ legged_gym/legged_gym/envs/base/humanoid_robot.py
@@ -731,9 +731,11 @@ class HumanoidRobot(BaseTask):
         # imu_obs = torch.stack((self.roll, self.pitch), dim=1)
         self.delta_yaw = wrap_to_pi(self.commands[:, 3] - self.yaw)
         # self.delta_next_yaw = wrap_to_pi(self.next_target_yaw - self.yaw)
-        self.delta_pose_x = self.cur_goals[:, 0] - self.root_states[:, 0]
-        self.delta_pose_y = self.cur_goals[:, 1] - self.root_states[:, 1]
-                
+        # self.delta_pose_x = self.cur_goals[:, 0] - self.root_states[:, 0]
+        # self.delta_pose_y = self.cur_goals[:, 1] - self.root_states[:, 1]
+        self.delta_pose_x = torch.zeros(self.num_envs, device=self.device)
+        self.delta_pose_y = torch.zeros(self.num_envs, device=self.device)            
+    
         noisy_delta_yaw = self.get_noisy_measurement(
             self.delta_yaw, 
             self.cfg.noise.noise_scales.delta_yaw
