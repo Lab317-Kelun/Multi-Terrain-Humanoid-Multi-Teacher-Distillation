55d6bc242c7a69140d5b870d267642e35192ebf2
xiezik <xie13318782539@163.com>
2025-12-13 21:20:12 +0800

    stage0_target_navigation_commonobs_height

--- legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
+++ legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
@@ -251,7 +251,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         speed_roughness_weight = 0.2  # 地形粗糙度权重
         class ranges( LeggedRobotCfg.commands.ranges ):
             lin_vel_x = [-0.8, 1.0] # min max [m/s]
-            lin_vel_y = [-0.0, 0.0]   # min max [m/s]
+            lin_vel_y = [-0.5, 0.5]   # min max [m/s]
             ang_vel_yaw = [-0.0, 0.0]    # min max [rad/s]
             heading = [-1.2, 1.2] 
             height = [-0.5, 0.0]
