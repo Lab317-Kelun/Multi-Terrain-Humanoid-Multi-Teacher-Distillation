691c2fe25c66b3509e611d54d4ce7ca5dee25875
xiezik <xie13318782539@163.com>
2025-12-15 14:11:50 +0800

    stage1_target_navigation_newobs_stand

--- legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
+++ legged_gym/legged_gym/envs/humanoid/humanoid_beamdojo_config.py
@@ -56,7 +56,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
     class env(LeggedRobotCfg.env):
         num_envs = 2048
         num_dofs = 12     # 机器人总自由度：只使用下肢12个关节（上肢已固定）
-        episode_length_s = 45.0 #与课程学习有关 
+        episode_length_s = 60.0 #与课程学习有关 45
         
         n_scan = 225
         n_priv = 3
@@ -78,7 +78,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         next_goal_threshold = 0.4
         reach_goal_delay = 0.1
         num_future_goal_obs = 2
-        goal_stop_duration = 2.0  # 到达目标后停止的持续时间（秒），停止期间观测的速度、角速度、误差都为0
+        goal_stop_duration = 5.0  # 到达目标后停止的持续时间（秒），停止期间观测的速度、角速度、误差都为0
         
     # 课程学习配置
     class curriculum_config:
@@ -100,7 +100,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
         next_goal_threshold_success_rate = 0.7  # 成功率阈值，超过此值才降低阈值
          
         # 存活时间模式参数
-        survival_time_threshold = 45.0  # 存活时间阈值（秒）
+        survival_time_threshold = 60.0  # 存活时间阈值（秒）
         survival_success_threshold = 3  # 连续存活成功次数阈值
         survival_failure_threshold = 2   # 连续存活失败次数阈值
         
@@ -312,7 +312,7 @@ class HumanoidBEAMDOJOCfg(LeggedRobotCfg):
             stand_still_vel = -0.3
             
             # foothold = 0.05
-            goal_reached = 1.0 #10
+            goal_reached = 10.0 #tele10 1
             
         only_positive_rewards = False
         tracking_sigma = 0.25
